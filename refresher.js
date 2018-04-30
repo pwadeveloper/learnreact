3f6d43a5ab81551668c47311000b686bea923cdb
pwadeveloper <sirmudiadavid@gmail.com>
2018-04-30 16:20:01 +0200

    Update

--- refresher.js
+++ refresher.js
@@ -28,3 +28,21 @@ person.printName();
 person.printGender();
 
 
+//spread and rest operator
+// spread for arrays
+const numbers = [1,2,3];
+const newNumbers = [...numbers, 4];
+
+console.log(newNumbers);
+
+//spread for objects
+const individual = {
+    name: 'Imasuen'
+};
+
+const newPerson = {
+    ...individual,
+    age: 28
+}
+
+console.log(newPerson);
